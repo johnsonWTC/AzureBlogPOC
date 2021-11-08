770bebba7da3bea6030bb280ff8fb1464d46936e
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-08 11:48:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureBlogPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
