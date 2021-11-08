03ad79940f227781c2cb26dab76215fda0f36f98
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-08 13:02:14 +0200

    Blob

--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ namespace AzureBlogPOC
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            await AzureBlobClient.UploadBlob();
         }
         static IConfigurationRoot GetConfigurationRoot()
         => new ConfigurationBuilder()
@@ -24,19 +25,15 @@ namespace AzureBlogPOC
         {
             public static async Task UploadBlob()
             {
-                var connectionString = "<Enter the connection string here>";
-                string containerName = "blobcontainer";
+                var connectionString = "DefaultEndpointsProtocol=https;AccountName=cvportaltest;AccountKey=yYsdmZrNMiJGpVzlwwXNU8Bu1X9b3AuCUo0wuf5lLHQ4e1FilYNonG3pUYtMGl40HFXRWypU4LnMyWERLPkbkA==;EndpointSuffix=core.windows.net";
+                string containerName = "videos";
                 var serviceClient = new BlobServiceClient(connectionString);
                 var containerClient = serviceClient.GetBlobContainerClient(containerName);
                 const string JsonDoc = "./Docs/";
                 string folderName = JsonDoc;
-                var candidateID = "testfile";
+                var candidateID = "stumza.txt";
                 string textFileLocation = folderName + candidateID + ".txt";
-
-
-
-                var fileName = "testfile.txt";
-             //   var localFile = Path.Combine(path, fileName);
+                var fileName = "stumza.txt";
                 await File.WriteAllTextAsync(textFileLocation, "This is a test message");
                 var blobClient = containerClient.GetBlobClient(fileName);
                 Console.WriteLine("Uploading to Blob storage");
@@ -45,4 +42,5 @@ namespace AzureBlogPOC
                 uploadFileStream.Close();
             }
         }
+    }
 }
